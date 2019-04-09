58df49e3eee7e92dc46e9cf029c26fceb1173057
张鹏 <scbeta@qq.com>
2019-04-09 14:34:28 +0800

    让集合控件中显示的类名称可以由DisplayNameAttribute修改。

---
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/CollectionControl/Implementation/Converters/TypeNameConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Xceed.Wpf.Toolkit.Converters
+{
+    public class TypeNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var type = value as Type;
+            if (type == null)
+                return null;
+            var attr = type.GetCustomAttributes(typeof(DisplayNameAttribute), false)
+                .Select(t => (DisplayNameAttribute)t)
+                .FirstOrDefault();
+            if (attr != null)
+                return attr.DisplayName;
+            return type.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
